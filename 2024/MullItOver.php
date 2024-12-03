fb368c8aac84717dd6ffad7e6a85c5292870c48f
Kayla Camacho <73963922+kcamach1@users.noreply.github.com>
2024-12-03 10:38:53 -0500

    placeholder

---
+++ 2024/MullItOver.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once dirname(__DIR__) . '/AdventOfCode.php';
+
+class MullItOver extends AdventOfCode
+{
+	protected int $day = 3;
+	protected int $year = 2024;
+	protected string $title = 'Mull It Over';
+
+	public function solve_part_one(): void
+	{
+		echo 'Part 1: TODO';
+	}
+}
+
+$puzzle = new MullItOver();
+$puzzle->solve_part_one();
