9f099097c4a8e9cf6ed793260cd9faa0ee0511eb
Kayla Camacho <73963922+kcamach1@users.noreply.github.com>
2024-12-04 15:41:37 -0500

    day 4 todo

---
+++ 2024/CeresSearch.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once dirname(__DIR__) . '/AdventOfCode.php';
+
+class CeresSearch extends AdventOfCode
+{
+	protected int $day = 4;
+	protected int $year = 2024;
+	protected string $title = "Ceres Search";
+
+	protected function solve_part_one(): string
+	{
+	    return 'TODO';
+	}
+
+//	protected function solve_part_two(): string
+//	{
+//		return 'TODO';
+//	}
+}
