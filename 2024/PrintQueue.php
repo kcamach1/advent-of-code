7008b2860d49631cb4662699a7274a0d5acf4f65
Kayla Camacho <73963922+kcamach1@users.noreply.github.com>
2024-12-06 14:13:44 -0500

    day 5 todo

---
+++ 2024/PrintQueue.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once dirname(__DIR__) . '/AdventOfCode.php';
+
+class PrintQueue extends AdventOfCode
+{
+	protected int $day = 5;
+	protected int $year = 2024;
+	protected string $title = "Print Queue";
+
+    // runs before solve_part_one() and solve_part_two()
+    protected function setup(): void
+    {
+        //
+    }
+
+	protected function solve_part_one(): string
+	{
+	    return 'TODO';
+	}
+
+//	protected function solve_part_two(): string
+//	{
+//		return 'TODO';
+//	}
+}
